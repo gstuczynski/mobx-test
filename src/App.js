538f18fc628bfd7413be1d9ea50d370a7adc33a0
g_stuczynski <grzegorz.stuczynski@gisonline.pl>
2019-06-19 15:25:03 +0200

    implement mobx

--- src/App.js
+++ src/App.js
@@ -1,38 +1,69 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { decorate, observable } from "mobx"
+import { observer } from "mobx-react"
 
-const Row = props => {
-  return (
-    <tr>
-      <td>{props.data.name}</td>
-      <td>{props.data.salary}</td>
-    </tr>
-  );
-};
+class Store {
+  employeesList = [
+    { name: "John Doe", salary: 150 },
+    { name: "Richard Roe", salary: 225 },
+  ]
+}
+
+decorate(Store, {
+  employeesList: observable
+})
+
+const appStore = new Store()
+
+const Row = (props) => {
+  return (<tr>
+    <td>{props.data.name}</td>
+    <td>{props.data.salary}</td>
+  </tr>)
+}
 
 class Table extends Component {
   render() {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <td>Name:</td>
-            <td>Daily salary:</td>
-          </tr>
-        </thead>
-        <tbody />
-      </table>
-    );
+    const { store } = this.props
+    return (<table>
+      <thead>
+        <tr>
+          <td>Name:</td>
+          <td>Daily salary:</td>
+        </tr>
+      </thead>
+      <tbody>
+        {store.employeesList.map((e, i) =>
+          <Row
+            key={i}
+            data={e}
+          />
+        )}
+      </tbody>
+    </table>)
   }
 }
+Table = observer(Table)
+
 
 class Controls extends Component {
+  addEmployee = () => {
+    const name = prompt("The name:")
+    const salary = parseInt(prompt("The salary:"), 10)
+    this.props.store.employeesList.push({ name, salary })
+    // ERROR !!! this will not update the view 
+  }
+
+  clearList = () => {
+    this.props.store.employeesList = []
+    // ERROR !!! this will not update the view 
+  }
+
   render() {
-    return (
-      <div className="controls">
-        <button>clear table</button>
-        <button>add record</button>
-      </div>
-    );
+    return (<div className="controls">
+      <button onClick={this.clearList}>clear table</button>
+      <button onClick={this.addEmployee}>add record</button>
+    </div>)
   }
 }
 
@@ -41,10 +72,10 @@ class App extends Component {
     return (
       <div>
         <h1>Mobx Table</h1>
-        <Controls />
-        <Table />
+        <Controls store={appStore} />
+        <Table store={appStore} />
       </div>
-    );
+    )
   }
 }
 
